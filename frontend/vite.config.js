26b13a06f008c06b4ed28be9d19d700185a5c6e5
shinsh <shinsh65@gmail.com>
2025-07-09 17:47:12 +0900

    Add initial files for Gallery App including assets, favicon, images, and index.html
    - Created orderService-BlWX2cgg.js for order-related API calls.
    - Added favicon.ico for the application icon.
    - Included multiple image files (001.jpg to 006.jpg) for the gallery.
    - Created index.html as the main entry point for the application with Bootstrap integration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/v1/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true
+// console.log(import.meta.env);
+export default defineConfig(({ mode }) => {
+  // console.log('mode:', mode);
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log('env:', env);
+  return {
+    server: {
+      proxy: {
+        "/v1/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
       }
-    }
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
